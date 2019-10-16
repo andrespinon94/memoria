0ffbbe5d03b7dbb17c1da4e03f53a8c3e697399b
Andres <andrespinon94@outlook.com>
2019-10-16 13:03:36 +0200

    // creación del array de imágenes para mostrar

--- memoria/ViewController.swift
+++ memoria/ViewController.swift
@@ -1,12 +1,19 @@
 
 import UIKit
 
+
+var img_array : [UIImage] = [#imageLiteral(resourceName: "FL4K"),#imageLiteral(resourceName: "moze"),#imageLiteral(resourceName: "Zane"),#imageLiteral(resourceName: "amara"),#imageLiteral(resourceName: "zero"),#imageLiteral(resourceName: "Gaige"),#imageLiteral(resourceName: "salvador"),#imageLiteral(resourceName: "Maya"),#imageLiteral(resourceName: "Axton")]
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var img_to_show: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    
 
 
 }
