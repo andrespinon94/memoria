7a9c341369d219d425638cf264aca99a7dba0dfc
Andres <andrespinon94@outlook.com>
2019-10-18 12:41:16 +0200

    // mostrar imágenes aleatorias

--- memoria/ViewController.swift
+++ memoria/ViewController.swift
@@ -9,16 +9,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var img_to_show: UIImageView!
     @IBOutlet weak var Start_button: UIButton!
     var img_array : [UIImage] = [#imageLiteral(resourceName: "FL4K"),#imageLiteral(resourceName: "moze"),#imageLiteral(resourceName: "Zane"),#imageLiteral(resourceName: "amara"),#imageLiteral(resourceName: "zero"),#imageLiteral(resourceName: "Gaige"),#imageLiteral(resourceName: "salvador"),#imageLiteral(resourceName: "Maya"),#imageLiteral(resourceName: "Axton")]
+    var shufled_array : [UIImage] = []
+    @IBOutlet weak var contador: UILabel!
+    
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
+    super.viewDidLoad()
+    shufled_array = img_array.shuffled()
+        contador.text = (" imagen 0 de  9 ")
         
-        
-
     }
     
     @IBAction func Start(_ sender: Any) {
         
-        img_to_show.image = img_array[0]
+    var i : Int = 0
+    var i_text : String
+        
+    Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true, block: { timer in
+        if i <= 8{
+    self.img_to_show.image = self.shufled_array[i]
+     i_text = String(i)
+            
+    contador.text = ((i)(" /  9 "))
+        i+=1
+            
+        }
+        })
+       
+        
+        
     }
     
 
